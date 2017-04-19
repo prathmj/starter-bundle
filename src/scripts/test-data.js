61fb5112af160522fde81879fd7892144a1f7a9c
Rodney Jean-Baptiste <rodneybaptiste@gmail.com>
2017-04-19 16:33:08 -0400

    remove broken image url

--- src/scripts/test-data.js
+++ src/scripts/test-data.js
@@ -1,18 +1,8 @@
-/* eslint-disable */
-
-/*********************************************************************************************************/
-/*------silo test data---------------------*/
-/* Replace the contents value of TEST_DATA */
-/* to match the format of your production  */
-/* data. more info: https://github.com/LinkNYC/starter-bundle/wiki/Data-Integration-%E2%80%A2-Silo-Data  */
-/*********************************************************************************************************/
-
 const TEST_DATA = [
   {"_device_id": "*", "id": "b76b1e6e-0818-41d3-b77d-7ccf414961ef", "url": "https://s3.amazonaws.com/dev.assets.vistarmedia.com/creative/1oprfWZYSRy3ZidxoVxK5g/71/197/9fdb6b31-dfe0-45ba-82f5-e1d213722fd6.png", "_index": "1", "_update_date": "2016-08-09T19:34:01.477000+00:00"},
   {"_device_id": "*", "id": "e5caae3b-964e-44b4-bb5d-21ee31b1d19d", "url": "https://s3.amazonaws.com/dev.assets.vistarmedia.com/creative/1oprfWZYSRy3ZidxoVxK5g/de/6e4/d75308bc-a674-4eaa-9a30-bef92eade350.png", "_index": "1", "_update_date": "2016-08-09T19:34:01.477000+00:00"},
   {"_device_id": "*", "id": "9d789b3d-4473-4dc2-8784-2e0ec385af32", "url": "https://s3.amazonaws.com/dev.assets.vistarmedia.com/creative/1oprfWZYSRy3ZidxoVxK5g/98/9a1/29abfa57-921d-4c47-b6d2-014ae7519c04.jpg", "_index": "1", "_update_date": "2016-08-09T19:34:01.477000+00:00"},
-  {"_device_id": "*", "id": "9d286c0d-6b6f-4915-9f3d-ee8e762e204a", "url": "https://s3.amazonaws.com/dev.assets.vistarmedia.com/creative/1oprfWZYSRy3ZidxoVxK5g/6f/221/f331610b-c7d7-44e8-8fbd-c0b0e5d50f54.jpg", "_index": "1", "_update_date": "2016-08-09T19:34:01.477000+00:00"},
-  {"_device_id": "*", "id": "9d286c0d-6b6f-4915-9f3d-ee8e762e203a", "url": "https://s3.amazonaws.com/tardis-adops-assets/test/s3.jpg", "_index": "1", "_update_date": "2016-08-09T19:34:01.477000+00:00"}
+  {"_device_id": "*", "id": "9d286c0d-6b6f-4915-9f3d-ee8e762e204a", "url": "https://s3.amazonaws.com/dev.assets.vistarmedia.com/creative/1oprfWZYSRy3ZidxoVxK5g/6f/221/f331610b-c7d7-44e8-8fbd-c0b0e5d50f54.jpg", "_index": "1", "_update_date": "2016-08-09T19:34:01.477000+00:00"}
 ];
 
 module.exports = TEST_DATA;
